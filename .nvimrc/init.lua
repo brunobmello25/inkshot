1ec1b2cb43dbc9febad7b130c9bfd45adc3217d4
Bruno Mello <bruno.barros.mello@gmail.com>
2026-06-29 12:38:15 -0300

    setup odin

--- .nvimrc/init.lua
+++ .nvimrc/init.lua
@@ -9,10 +9,22 @@ local dap_c_config = {
 		name = "Debug Game (codelldb)",
 		type = "codelldb",
 		request = "launch",
-		program = vim.fn.getcwd() .. "/target/inkshot",
-		cwd = vim.fn.getcwd() .. "/target",
+		program = vim.fn.getcwd() .. "/data/inkshot",
+		cwd = vim.fn.getcwd() .. "/data",
 		stopOnEntry = false,
 	},
 }
+
 dap.configurations.c = dap_c_config
 dap.configurations.cpp = dap_c_config
+
+dap.configurations.odin = {
+	{
+		name = "Debug (codelldb)",
+		type = "codelldb",
+		request = "launch",
+		program = vim.fn.getcwd() .. "/data/inkshot",
+		cwd = vim.fn.getcwd() .. "/data",
+		stopOnEntry = false,
+	},
+}
