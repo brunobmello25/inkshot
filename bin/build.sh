6e06f09f771deff9d5873853e339f70999d05e51
Bruno Mello <bruno.barros.mello@gmail.com>
2026-08-24 23:04:17 -0300

    fix c wayland files

--- bin/build.sh
+++ bin/build.sh
@@ -7,10 +7,10 @@ wayland-scanner client-header ../protocols/xdg-shell.xml ../src/os/generated/xdg
 wayland-scanner private-code ../protocols/xdg-shell.xml ../src/os/generated/xdg-shell-client-protocol.c
 
 wayland-scanner client-header ../protocols/wlr-screencopy-unstable-v1.xml ../src/os/generated/wlr-screen-copy.h
-wayland-scanner client-header ../protocols/wlr-screencopy-unstable-v1.xml ../src/os/generated/wlr-screen-copy.c
+wayland-scanner private-code ../protocols/wlr-screencopy-unstable-v1.xml ../src/os/generated/wlr-screen-copy.c
 
 wayland-scanner client-header ../protocols/wlr-layer-shell-unstable-v1.xml ../src/os/generated/wlr-layer-shell.h
-wayland-scanner client-header ../protocols/wlr-layer-shell-unstable-v1.xml ../src/os/generated/wlr-layer-shell.c
+wayland-scanner private-code ../protocols/wlr-layer-shell-unstable-v1.xml ../src/os/generated/wlr-layer-shell.c
 
 gcc $(realpath ../src/app/wayland_main.c) -o inkshot \
     -DBUILD_SLOW=1 \
